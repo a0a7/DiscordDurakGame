33e12c5b09e935e54139537cdda742db0ca9adb1
levtus <i@alexander.systems>
2024-06-18 13:32:02 -0500

    Actually functioning discord integration!!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,14 @@
 
 <style lang="postcss">
     :global(body) {
+		background-color: #1F2B3F;
 		background-image: url('background_tile.png');
         background-repeat: repeat;
+		margin: 0px;
+        padding: 0px;
+        border: 0;
+        height: 100vh;
+        width: 100vw;
+        overflow: hidden;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
 <script lang="ts">
+    import { onMount } from "svelte";
+    import { DiscordHelper } from "$lib/utils/DiscordHelper";
+    import { config } from "$lib/config";
+
+    let discordHelper: DiscordHelper;
+
+    onMount(() => {
+        discordHelper = new DiscordHelper();
+        discordHelper.setupParentIframe();
+    });
 </script>
