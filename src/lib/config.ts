8297cd6573a96498a1bfe665c6694c42d7dbb17c
levtus <i@alexander.systems>
2024-06-18 11:37:47 -0500

    Helper script fragments

---
+++ src/lib/config.ts
@@ -0,0 +1,6 @@
+export const config = {
+    CLIENT_ID: "1252657987252654201", // Discord Application ID
+    COMPANY_NAME: "aaw", // Company or Developer Name
+    PRODUCT_NAME: "Durak", // Game Name
+    PRODUCT_VERSION: "0.1.0", // Game Version
+};
